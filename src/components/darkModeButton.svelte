1f3db89113177ec315cd8813d0db7b000a001bc1
lukaszwisniewski88 <fr.luksus@gmail.com>
2021-02-09 12:17:31 +0100

    Persisting theme and simple button
    Simple button copied from repl provided. It toggles the class on body tag and also in LocalStorage. I've used onMount method in main component to make sure the right color mode is applied on app load.

---
+++ src/components/darkModeButton.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { toggleTheme } from '../services/localTheme'
+  function toggle() {
+    window.document.body.classList.toggle('dark')
+    toggleTheme()
+  }
+</script>
+
+<button on:click={toggle}>
+  <slot />
+</button>
+
+<style>
+  button {
+    background-color: #f76027;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    padding: 0.5rem;
+    text-transform: uppercase;
+  }
+</style>
---
+++ src/services/localTheme.js
@@ -0,0 +1,30 @@
+const themeKey = 'theme'
+export const themes = {
+    dark:'dark',
+    light:'light'
+}
+
+function setInitialTheme(){
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+        //dark-mode Preffered
+        localStorage.setItem(themeKey, themes.dark)
+        return themes.dark
+    }else{
+        localStorage.setItem(themeKey, themes.light)
+        return themes.light
+    }
+}
+function setTheme(theme){
+    localStorage.setItem(themeKey, theme)
+}
+
+export function getLocalTheme(){
+     return localStorage.getItem(themeKey) ? localStorage.getItem(themeKey) : setInitialTheme()
+}
+export function toggleTheme(){
+    if(getLocalTheme() === themes.light){
+        setTheme(themes.dark)
+    }else{
+        setTheme(themes.light)
+    }
+}
