0c31228fe9e228c26e3fc1771bd50a842aa4dfe5
Chandler Baskins <baskinschandler@gmail.com>
2021-02-15 20:53:15 -0600

    BUG: check to see if we are clicking on input element before moving card/changed title element to input for better perf/ access

--- src/components/column.svelte
+++ src/components/column.svelte
@@ -4,6 +4,7 @@
     deleteKanbanColumn,
     reverseState,
     changePosition,
+    editColumnName
   } from '../store/board.store'
   import { draggable } from '../actions/draggable'
   import TitleInput from './column.titleInput.svelte'
@@ -12,8 +13,9 @@
   export let id
   export let title
   
-  let isTitleEditing = false
   let isDragged = false
+
+
 </script>
 
 <div
@@ -27,16 +29,11 @@
   }}
   on:dropped
 >
-  <header
-    class="text-sm font-thin h-5 border-b-2 border-blue-600 overflow-hidden cursor-text"
-    on:click={() => (isTitleEditing = true)}
+
+
+  <input on:blur="{(e) => editColumnName(id,e.target.value)}" type="text" 
+    class="text-sm font-thin h-5 border-b-2 border-blue-600 overflow-hidden cursor-text bg-transparent focus:outline-none"
   >
-    {#if isTitleEditing}
-      <TitleInput {title} {id} on:blur={() => (isTitleEditing = false)} />
-    {:else}
-      {title}
-    {/if}
-  </header>
 
   <div class="flex-1">
     <slot>
