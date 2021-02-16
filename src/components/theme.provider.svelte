c05267a151ed65ee75f17c019fa4c4bf641c67c7
lukaszwisniewski88 <fr.luksus@gmail.com>
2021-02-16 17:55:23 +0100

    fixed a typo in theme store
    Typo

--- src/components/theme.provider.svelte
+++ src/components/theme.provider.svelte
@@ -1,11 +1,9 @@
 <script>
   import { onMount } from 'svelte'
   import { themeStore, themes } from '../store/theme.store'
-  import { persistTheme } from '../services/localStorage'
 
   onMount(() => {
     const unsunscribe = themeStore.subscribe((theme) => {
-      persistTheme(theme)
       if (theme === themes.dark) {
         window.document.documentElement.className = themes.dark
       } else {
