eca60858cd6f0817abe09c4cd2bdfaca0e78766c
brittneypostma <brittney.postma@hotmail.com>
2021-02-20 12:02:42 -0500

    added colors to config and updated a few colors in styles

--- src/components/kanbanBoard.svelte
+++ src/components/kanbanBoard.svelte
@@ -8,7 +8,7 @@
 >
   <slot />
   <button
-    class="relative flex-shrink-0 w-64 p-2 text-xs border-2 border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-accent"
+    class="relative flex-shrink-0 w-32 p-2 text-xs transition-colors border-2 border-gray-500 rounded focus:outline-none focus:ring-2 focus:ring-accent hover:bg-gray-100 dark:hover:bg-gray-800"
     on:click={addKanbanColumn}
   >
     Add New Column
