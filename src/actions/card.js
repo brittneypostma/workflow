fe741cbcf850d66f7116a97c67901087d432bb07
usama v <varikkottilusama@gmail.com>
2021-02-23 11:12:44 +0530

    refactor "add card" function, add "delete card" function

--- src/actions/card.js
+++ src/actions/card.js
@@ -1,7 +1,20 @@
 import {board} from "../store/board.store"
-export function addCard(id, card) {
+export function addCard(event) {
+    const { column: columnId, card } = event.detail
+
     board.update(state => state.map(column => {
-        column.id === id && column.cards.push(card);
+        column.id === columnId && column.cards.push(card);
+        return ({ ...column })
+    }))
+}
+
+export function deleteCard(event) {
+    const { column: columnId, card } = event.detail
+    card = JSON.stringify(card);
+    board.update(columns => columns.map(column => {
+        if(column.id == columnId) {
+            column.cards = column.cards.filter(c => c != card);
+        }
         return ({ ...column })
     }))
 }
